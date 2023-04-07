d1f9283c9df3e4450cd15da03602dc19655d6920
VVervolk <113511730+VVervolk@users.noreply.github.com>
2023-04-07 11:25:45 +0000

    Deploying to gh-pages from @ VVervolk/goit-js-hw-09@86e3751172f0725879e61167b98db47a9bd9ae19 🚀

---
+++ 01-color-switcher.d8e852a8.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let d=null;e.setAttribute("disabled",!0),t.addEventListener("click",(()=>{d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(()=>{clearInterval(d),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.d8e852a8.js.map
